4686b1b87f1627d290d87d81cee5845d448e82e2
Yemmieeyc <adewole_aina@yahoo.com>
2024-10-04 09:46:02 -0700

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,36 @@
 // STRETCH
+const cars = [{
+    vin: '1234567891234',
+    make: 'toyota',
+    model: 'camry',
+    milage: '20000',
+    title: 'clean',
+    transmission: 'manual',
+},
+{
+    vin: '1234567891234',
+    make: 'toyota',
+    model: 'corolla',
+    milage: '203300',
+    title: 'salvage',
+    transmission: 'manual',
+},
+{
+    vin: '1234567891234',
+    make: 'Benz',
+    model: 'c300',
+    milage: '203900',
+    
+},
+]
+// exports.seed = function(knex){
+//     return knex('cars')
+//     .truncate().then(()=> {
+//         return knex('cars').insert(cars)
+//     })
+    
+// }
+exports.seed = async function(knex){
+    await knex('cars').truncate()
+    await knex ('cars').insert(cars)
+}
